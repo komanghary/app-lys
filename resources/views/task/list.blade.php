8b8d457789fad807ae4d65bd4d80ced16d9f5897
Komang <komanghary99@gmail.com>
2025-04-06 12:57:32 +0800

    U btn hapus list task

--- resources/views/task/list.blade.php
+++ resources/views/task/list.blade.php
@@ -45,7 +45,10 @@
                             </td>
                             <td class="px-6 py-4 whitespace-nowrap text-sm font-medium table-fixed">
                                 <a href="#"
-                                    class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Default</a>
+                                    class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Edit</a>
+                                <a href="" type="button"
+                                    class="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900">Hapus</a>
+
                             </td>
                         </tr>
 
